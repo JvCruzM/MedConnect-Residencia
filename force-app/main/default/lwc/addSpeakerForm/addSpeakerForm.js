e270309521d8223131e5229c51a5537365daa6c4
JvCruzM <cruzdemenezesjv@gmail.com>
2026-06-14 15:19:39 -0300

    Adiciona funcionalidades de edição e exclusão de palestrantes, incluindo carregamento de dados para edição e validações de permissão

--- force-app/main/default/lwc/addSpeakerForm/addSpeakerForm.js
+++ force-app/main/default/lwc/addSpeakerForm/addSpeakerForm.js
@@ -1,16 +1,44 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
-import { NavigationMixin } from 'lightning/navigation';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+
+import getSpeakerForEdit from '@salesforce/apex/FeaturedSpeakersController.getSpeakerForEdit';
+import updateSpeaker from '@salesforce/apex/FeaturedSpeakersController.updateSpeaker';
 
 export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
     @track speakerData = {};
 
+    speakerId;
     errorMessage = '';
     successMessage = '';
     isSaving = false;
 
+    @wire(CurrentPageReference)
+    getPageReference(pageRef) {
+        if (pageRef && pageRef.state) {
+            const pageSpeakerId = pageRef.state.c__speakerId || pageRef.state.speakerId;
+
+            if (pageSpeakerId && pageSpeakerId !== this.speakerId) {
+                this.speakerId = pageSpeakerId;
+                this.loadSpeakerForEdit();
+            }
+        }
+    }
+
+    get isEditMode() {
+        return !!this.speakerId;
+    }
+
+    get formTitle() {
+        return this.isEditMode ? 'Editar Palestrante' : 'Adicionar Palestrante';
+    }
+
     get saveButtonLabel() {
-        return this.isSaving ? 'Salvando palestrante...' : 'Salvar Palestrante';
+        if (this.isSaving) {
+            return this.isEditMode ? 'Salvando alterações...' : 'Salvando palestrante...';
+        }
+
+        return this.isEditMode ? 'Salvar Alterações' : 'Salvar Palestrante';
     }
 
     get profileUrl() {
@@ -19,6 +47,27 @@ export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
 
     get hasPhotoUrl() {
         return !!this.profileUrl;
+    }
+
+    loadSpeakerForEdit() {
+        this.errorMessage = '';
+        this.successMessage = '';
+
+        getSpeakerForEdit({ speakerId: this.speakerId })
+            .then(data => {
+                this.speakerData = {
+                    Name: data.nameValue,
+                    Email__c: data.email,
+                    Phone__c: data.phone,
+                    Specialisation__c: data.specialisation,
+                    Profile_URL__c: data.profileUrl,
+                    About_Me__c: data.aboutMe
+                };
+            })
+            .catch(error => {
+                this.errorMessage = this.extractErrorMessage(error);
+                console.error('Erro ao carregar palestrante:', JSON.parse(JSON.stringify(error)));
+            });
     }
 
     handleChange(event) {
@@ -77,6 +126,14 @@ export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
 
         this.isSaving = true;
 
+        if (this.isEditMode) {
+            this.updateExistingSpeaker();
+        } else {
+            this.createNewSpeaker();
+        }
+    }
+
+    createNewSpeaker() {
         const fields = this.buildFields();
 
         const recordInput = {
@@ -87,17 +144,9 @@ export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
         createRecord(recordInput)
             .then(() => {
                 this.successMessage = 'Palestrante adicionado com sucesso.';
-
-                setTimeout(() => {
-                    this[NavigationMixin.Navigate]({
-                        type: 'comm__namedPage',
-                        attributes: {
-                            name: 'Speakers__c'
-                        }
-                    });
-                }, 1000);
+                this.navigateToSpeakers();
             })
-            .catch((error) => {
+            .catch(error => {
                 this.errorMessage = this.extractErrorMessage(error);
                 console.error('Detalhe Técnico:', JSON.parse(JSON.stringify(error)));
             })
@@ -106,10 +155,44 @@ export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
             });
     }
 
+    updateExistingSpeaker() {
+        updateSpeaker({
+            speakerId: this.speakerId,
+            nameValue: this.speakerData.Name,
+            email: this.speakerData.Email__c,
+            phone: this.speakerData.Phone__c,
+            specialisation: this.speakerData.Specialisation__c,
+            profileUrl: this.speakerData.Profile_URL__c,
+            aboutMe: this.speakerData.About_Me__c
+        })
+            .then(() => {
+                this.successMessage = 'Palestrante atualizado com sucesso.';
+                this.navigateToSpeakers();
+            })
+            .catch(error => {
+                this.errorMessage = this.extractErrorMessage(error);
+                console.error('Erro ao atualizar palestrante:', JSON.parse(JSON.stringify(error)));
+            })
+            .finally(() => {
+                this.isSaving = false;
+            });
+    }
+
+    navigateToSpeakers() {
+        setTimeout(() => {
+            this[NavigationMixin.Navigate]({
+                type: 'comm__namedPage',
+                attributes: {
+                    name: 'Speakers__c'
+                }
+            });
+        }, 1000);
+    }
+
     buildFields() {
         const fields = {};
 
-        Object.keys(this.speakerData).forEach((fieldName) => {
+        Object.keys(this.speakerData).forEach(fieldName => {
             const value = this.speakerData[fieldName];
 
             if (typeof value === 'string') {
@@ -131,6 +214,10 @@ export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
             return 'Erro desconhecido ao salvar palestrante.';
         }
 
+        if (error.body && error.body.message) {
+            return error.body.message;
+        }
+
         if (error.body && error.body.output) {
             if (error.body.output.errors && error.body.output.errors.length > 0) {
                 return error.body.output.errors[0].message;
@@ -144,10 +231,6 @@ export default class AddSpeakerForm extends NavigationMixin(LightningElement) {
                     return `Erro no campo ${firstField}: ${fieldErrors[firstField][0].message}`;
                 }
             }
-        }
-
-        if (error.body && error.body.message) {
-            return error.body.message;
         }
 
         if (error.message) {
--- force-app/main/default/lwc/featuredSpeakers/featuredSpeakers.js
+++ force-app/main/default/lwc/featuredSpeakers/featuredSpeakers.js
@@ -1,46 +1,134 @@
 import { LightningElement, wire, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import { refreshApex } from '@salesforce/apex';
+
 import getAllSpeakers from '@salesforce/apex/FeaturedSpeakersController.getAllSpeakers';
+import deleteSpeaker from '@salesforce/apex/FeaturedSpeakersController.deleteSpeaker';
+
 import USER_ID from '@salesforce/user/Id';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import YOU_ARE_FIELD from '@salesforce/schema/User.You_Are__c';
 
 export default class FeaturedSpeakers extends NavigationMixin(LightningElement) {
     @track speakers = [];
+
+    rawSpeakers = [];
+    wiredSpeakersResult;
+
     error;
-    
+    errorMessage = '';
+    successMessage = '';
+
     isOrganizer = false;
 
     @wire(getRecord, { recordId: USER_ID, fields: [YOU_ARE_FIELD] })
     userData({ error, data }) {
         if (data) {
             const role = getFieldValue(data, YOU_ARE_FIELD);
-            this.isOrganizer = (role === 'Organizer');
+            this.isOrganizer = role === 'Organizer';
+            this.applySpeakerPermissions();
         } else if (error) {
             console.error('Erro ao verificar permissão do usuário', error);
         }
     }
 
     @wire(getAllSpeakers)
-    wiredSpeakers({ error, data }) {
+    wiredSpeakers(result) {
+        this.wiredSpeakersResult = result;
+
+        const { error, data } = result;
+
         if (data) {
-            this.speakers = data;
+            this.rawSpeakers = data;
+            this.applySpeakerPermissions();
             this.error = undefined;
+            this.errorMessage = '';
         } else if (error) {
             this.error = error;
+            this.rawSpeakers = [];
             this.speakers = [];
+            this.errorMessage = 'Não foi possível carregar os palestrantes.';
             console.error(error);
         }
     }
 
+    get hasSpeakers() {
+        return this.speakers && this.speakers.length > 0;
+    }
+
+    applySpeakerPermissions() {
+        this.speakers = (this.rawSpeakers || []).map(speaker => {
+            return {
+                ...speaker,
+                canManage: this.isOrganizer && speaker.CreatedById === USER_ID
+            };
+        });
+    }
+
     handleAddSpeaker(event) {
         event.preventDefault();
-        
+
         this[NavigationMixin.Navigate]({
             type: 'comm__namedPage',
             attributes: {
-                name: 'Add_Speaker__c' 
+                name: 'Add_Speaker__c'
             }
         });
+    }
+
+    handleEditSpeaker(event) {
+        const speakerId = event.currentTarget.dataset.id;
+
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Add_Speaker__c'
+            },
+            state: {
+                c__speakerId: speakerId
+            }
+        });
+    }
+
+    handleDeleteSpeaker(event) {
+        const speakerId = event.currentTarget.dataset.id;
+        const speakerName = event.currentTarget.dataset.name;
+
+        const confirmed = window.confirm(
+            `Tem certeza que deseja excluir o palestrante "${speakerName}"?`
+        );
+
+        if (!confirmed) {
+            return;
+        }
+
+        this.errorMessage = '';
+        this.successMessage = '';
+
+        deleteSpeaker({ speakerId })
+            .then(() => {
+                this.successMessage = 'Palestrante excluído com sucesso.';
+                return refreshApex(this.wiredSpeakersResult);
+            })
+            .catch(error => {
+                this.errorMessage = this.extractErrorMessage(error);
+                console.error('Erro ao excluir palestrante:', JSON.parse(JSON.stringify(error)));
+            });
+    }
+
+    extractErrorMessage(error) {
+        if (!error) {
+            return 'Erro desconhecido.';
+        }
+
+        if (error.body && error.body.message) {
+            return error.body.message;
+        }
+
+        if (error.message) {
+            return error.message;
+        }
+
+        return 'Erro desconhecido.';
     }
 }
